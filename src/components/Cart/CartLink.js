ec2db3cc2101fe2d485de2602cc7ba35747724f1
krittiyaclark <krittiyaclark@gmail.com>
2020-07-14 12:53:59 -0400

    Project foldders setup

---
+++ src/components/Cart/CartLink.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CartLink = () => {
+	return <div></div>
+}
+
+export default CartLink
---
+++ src/components/LoginLink.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LoginLink = () => {
+	return <div></div>
+}
+
+export default LoginLink
