a273ac0fdc40261274065eb41529c2231d87249c
krittiyaclark <krittiyaclark@gmail.com>
2020-07-21 08:35:01 -0400

    Changed Hero background image & background font color

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = ({ children }) => {
 	return (
 		<div className='hero'>
 			<div className='banner'>
-				<h1>think, code, deploy</h1>
+				<h1>find vintage tech products that you like</h1>
 				<p>embrace your choices - we do</p>
 				{children}
 			</div>
